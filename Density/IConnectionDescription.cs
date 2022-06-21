77e9ff26c72ddcd547bb39a282e5601c4445ca22
tacosontitan <jdavis@globallcs.com>
2022-06-21 11:47:25 -0500

    first commit

---
+++ Density/IConnectionDescription.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Density
+{
+    public interface IConnectionDescription
+    {
+
+        #region Properties
+
+        /// <summary>
+        /// The server where the database is hosted.
+        /// </summary>
+        string Server { get; set; }
+        /// <summary>
+        /// The name of the database to connect to.
+        /// </summary>
+        string Database { get; set; }
+        /// <summary>
+        /// The username that should be used to authenticate to the database.
+        /// </summary>
+        string Username { get; set; }
+        /// <summary>
+        /// The password used to authenticate to the database.
+        /// </summary>
+        string Password { get; set; }
+        /// <summary>
+        /// The length of time connections are allowed to run without a response before timing out.
+        /// </summary>
+        TimeSpan? Timeout { get; set; }
+
+        #endregion
+
+    }
+}
