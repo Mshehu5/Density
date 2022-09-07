c54c6f00851aa89ca721f11cea868c00b31d475c
tacosontitan <jdavis@globallcs.com>
2022-09-06 20:53:27 -0500

    Added empty summary tags to endpoint parameter.

--- Density/EndpointParameter.cs
+++ Density/EndpointParameter.cs
@@ -1,9 +1,23 @@
 ﻿namespace Density
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class EndpointParameter
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
         public object Value { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
         public EndpointParameter(string name, object value)
         {
             Name = name;
