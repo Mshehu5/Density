af4502845a0e35cbb420eadf72b66cb6511c01d0
tacosontitan <jdavis@globallcs.com>
2022-09-06 20:55:41 -0500

    Added empty summary tags to request builder interface.

--- Density/IRequestBuilder.cs
+++ Density/IRequestBuilder.cs
@@ -2,8 +2,16 @@
 
 namespace Density
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public interface IRequestBuilder : IDisposable
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="endpoint"></param>
+        /// <returns></returns>
         IDataRequest Build(Endpoint endpoint);
     }
 }
