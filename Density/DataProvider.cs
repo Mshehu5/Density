c97141d137b81ed7ef41648a67e8418602528569
tacosontitan <jdavis@globallcs.com>
2022-09-06 20:52:29 -0500

    Added empty summary tags to data provider.

--- Density/DataProvider.cs
+++ Density/DataProvider.cs
@@ -2,18 +2,32 @@
 
 namespace Density
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public abstract class DataProvider
     {
 
         #region Properties
 
+        /// <summary>
+        /// 
+        /// </summary>
         protected IClientBuilder ClientBuilder { get; private set; }
+        /// <summary>
+        /// 
+        /// </summary>
         protected IRequestBuilder RequestBuilder { get; private set; }
 
         #endregion
 
         #region Constructor
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="clientBuilder"></param>
+        /// <param name="requestBuilder"></param>
         public DataProvider(IClientBuilder clientBuilder, IRequestBuilder requestBuilder)
         {
             ClientBuilder = clientBuilder;
@@ -24,24 +38,57 @@ namespace Density
 
         #region Public Methods
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="endpoint"></param>
+        /// <returns></returns>
         public T Get<T>(Endpoint endpoint)
         {
             IDataResponse response = GetRaw(endpoint);
             return ParseResponse<T>(response);
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="endpoint"></param>
+        /// <returns></returns>
         public async Task<T> GetAsync<T>(Endpoint endpoint)
         {
             IDataResponse response = await GetRawAsync(endpoint);
             return ParseResponse<T>(response);
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="endpoint"></param>
+        /// <returns></returns>
         public IDataResponse GetRaw(Endpoint endpoint) => ExecuteEndpoint(endpoint);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="endpoint"></param>
+        /// <returns></returns>
         public async Task<IDataResponse> GetRawAsync(Endpoint endpoint) => await Task.Run(() => GetRaw(endpoint));
 
         #endregion
 
         #region Protected Methods
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="response"></param>
+        /// <returns></returns>
         protected abstract T ParseResponse<T>(IDataResponse response);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="endpoint"></param>
+        /// <returns></returns>
         protected virtual IDataResponse ExecuteEndpoint(Endpoint endpoint)
         {
             using (IDataClient client = ClientBuilder.Build(endpoint))
