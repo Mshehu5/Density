093bb22b3e01c22df24857b7f7c6506785d85151
tacosontitan <jdavis@globallcs.com>
2022-09-06 20:55:02 -0500

    Added empty summary tags to data response interface.

--- Density/IDataResponse.cs
+++ Density/IDataResponse.cs
@@ -2,9 +2,18 @@
 
 namespace Density
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public interface IDataResponse
     {
+        /// <summary>
+        /// 
+        /// </summary>
         HttpStatusCode Status { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
         string Content { get; set; }
     }
 }
