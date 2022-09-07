7106d923fa537696e13c76ad212992b72d608d54
Mshehu5 <104724920+Mshehu5@users.noreply.github.com>
2022-09-07 15:51:46 +0100

    Update summary tags for DatabaseInformation.cs
    I have populated summary tags to match the documentation for SqlConnectionStringBuilder

--- Density/Sql/DatabaseInformation.cs
+++ Density/Sql/DatabaseInformation.cs
@@ -16,35 +16,35 @@ namespace Density.Sql
         /// </summary>
         public bool UseIntegratedSecurity { get; set; }
         /// <summary>
-        /// 
+        /// Gets or sets a Boolean value that indicates whether User ID and Password are specified in the connection (when false) or whether the current Windows account credentials are used for authentication (when true).
         /// </summary>
         public string CurrentLanguage { get; set; }
         /// <summary>
-        /// 
+        /// Gets or sets the SQL Server Language record name
         /// </summary>
         public string Server { get; set; }
         /// <summary>
-        /// 
+        /// Gets or sets the name or network address of the instance of SQL Server to connect to.
         /// </summary>
         public string Database { get; set; }
         /// <summary>
-        /// 
+        /// Gets or sets the name of the database associated with the connection.
         /// </summary>
         public string Username { get; set; }
         /// <summary>
-        /// 
+        /// Gets or sets the user ID to be used when connecting to SQL Server.
         /// </summary>
         public string Password { get; set; }
         /// <summary>
-        /// 
+        /// Gets or sets the password for the SQL Server account.
         /// </summary>
         public TimeSpan Timeout { get; set; }
         /// <summary>
-        /// 
+        /// Gets or sets the length of time (in seconds) to wait for a connection to the server before terminating the attempt and generating an error.
         /// </summary>
         public TimeSpan LoadBalanceTimeout { get; set; }
         /// <summary>
-        /// 
+        /// Gets or sets the minimum time, in seconds, for the connection to live in the connection pool before being destroyed.
         /// </summary>
         [JsonIgnore] [XmlIgnore]
         public string ConnectionString
